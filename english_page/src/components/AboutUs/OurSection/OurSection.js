e9af52e91d53f016e793c45a18a6a1b4fc36a42b
Aijan <aikojune@gmail.com>
2018-06-25 10:54:29 +0600

    Add animation on scroll for OurSectionItems

--- english_page/src/components/AboutUs/OurSection/OurSection.js
+++ english_page/src/components/AboutUs/OurSection/OurSection.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import OurSectionItem from "./OurSectionItem/OurSectionItem";
 import './OurSection.css';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 const OurSection = props => (
     <div className="OurSection">
-        <OurSectionItem
-            bigNumber='01' title='Наша история'
-            text='стартап Neobis был основан в 2016 году двумя девушками, студентками-программистами АУЦА, Санирой и Бегимай.'
-        />
-        <OurSectionItem
-        bigNumber='02' title='Наша Идеология'
-        text='улучшение качества IT инфраструктуры в Кыргызстане через повышение профессионализма и подготовленности выпускников'
-        />
-        <OurSectionItem
-            bigNumber='03' title='Наша Миссия'
-            text='безвозмездное менторство по улучшению практических навыков и последующее трудоустройство в крупные  IT компании  Кыргызстана'
-        />
+        <ScrollAnimation
+            delay={100}
+            animateIn='fadeIn'
+            duration={3}
+            initiallyVisible={false}>
+            <OurSectionItem
+                bigNumber='01' title='Наша история'
+                text='стартап Neobis был основан в 2016 году двумя девушками, студентками-программистами АУЦА, Санирой и Бегимай.'
+            />
+        </ScrollAnimation>
+        <ScrollAnimation
+            delay={400}
+            animateIn='fadeIn'
+            duration={3}
+            initiallyVisible={false}>
+            <OurSectionItem
+                bigNumber='02' title='Наша Идеология'
+                text='улучшение качества IT инфраструктуры в Кыргызстане через повышение профессионализма и подготовленности выпускников'
+            />
+        </ScrollAnimation>
+        <ScrollAnimation
+            delay={700}
+            animateIn='fadeIn'
+            duration={3}
+            initiallyVisible={false}>
+            <OurSectionItem
+                bigNumber='03' title='Наша Миссия'
+                text='безвозмездное менторство по улучшению практических навыков и последующее трудоустройство в крупные  IT компании  Кыргызстана'
+            />
+        </ScrollAnimation>
     </div>
 );
 
